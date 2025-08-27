e492351dea028e81829753708847337ef9da0138
Clevison <cleversonmaiden031@gmail.com>
2025-08-26 22:33:59 -0500

    revisando

--- aula-6/aula-6.c
+++ aula-6/aula-6.c
@@ -1,38 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> //system
 int main(){
-   int nota;              // Para guardar a nota digitada
-    int quantidade = 0;    // Conta quantas notas foram digitadas
-    int soma = 0;          // Soma de todas as notas
-    char resposta;         // Guarda a resposta do usuário (s ou outra letra)
-    float media;           // Média das notas
+   int soma=0, nota, quantidade=0;
+   char resposta;
+   float media;
 
-    do
-    {
-       printf("\nDigite uma nota: ");
-       scanf("%d", &nota);
-
-       soma += nota;
-       quantidade++;
-
-       while(getchar() != '\n');
-       printf("\nAperte  C continuar ou 'S' para sair!");
-       resposta = getchar();
-
-       while(getchar() != '\n');
-
-       
-
-    } while (resposta != 'S' && resposta != 's');
-        media=(float)soma / quantidade;
-
-        printf("\nO valor da avaliação %d\n", quantidade);
-        printf("\nO valor das notas %d", soma);
-        printf("\nA média das notas %.2f\n", media);
-        printf("Aperte Enter para sair");
-        getchar();
-        
-        return 0;
+   do
+   {
+      printf("\n Digite uma nota");
+      scanf("%d", &nota);
+      soma += nota;
+      quantidade++;
 
 
+      while(getchar() != '\n'); //limpa após o Enter.
+      printf("\n Digite uma tecla para continuar ou 'S' para sair");
+      resposta = getchar();
+
+      while(getchar() != '\n');
+
+   } while (resposta != 'S' && resposta != 's');
+     media = (float) soma / quantidade;
+
+     printf("\n A quantidade seria %d\n", quantidade);
+     printf("\n As notas seria %d", soma);
+     printf("\n A média é %.2f\n", media);
+     printf("Aperte Enter para sair");
+     getchar();
+
+     return 0;
+   
 }
